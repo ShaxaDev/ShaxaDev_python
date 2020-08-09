443db78c5d2d3a5fae375def8e4121b12d5219aa
ShaxaDev <69134199+ShaxaDev@users.noreply.github.com>
2020-08-09 14:50:10 +0500

    Add files via upload

---
+++ baza.py
@@ -0,0 +1,10 @@
+t='Bot comandasi✅\n/start botni ishga tushirish uchun'
+
+#kk='python bu juda keng tarqalagan kuchli dasturlash tili hisoblanadi\nTkinter-desktop uchun dasturlar yaratish uchun'
+sayt ='TOP 10 ta saytlar to\'plami\n1. www.codecademy.com \n2. www.lynda.com \n3. www.udemy.com \n4. www.udacity.com \n5. www.coursera.org \n6. www.w3schools.com \n7. www.thenewboston.org \n8. www.programmr.com \n9. www.codeavengers.com \n10. www.codeschool.com '
+
+sertifikat="*⭕️🗣DIQQAT! Test ishlash qoidalari\n✅0-5 tagacha sertifikat berilmaydi\n✅6-8 tagacha yaxshi darajali\n✅9-10 ta yechsangiz a'lo 🥇 darajali sertifikat beriladi\nEslatma :ism familyangiz sertifikatga yoziladi shuning uchun to'g'ri kiriting\nTestlarda omad tilab qolgan holda ShaxaDev boshladik✔️ *"
+
+
+
+matn='Bu bot beginnerlar uchun pythonni o\'rganishga yordam beradi\nbot python dasturlash tilining pytelegrambotapi library yordamida yaratilgan\nbot hali rivojlantiriladi takliflaringini coderga jo\'nating'
